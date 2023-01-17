4f7229ffb09e4bd9311feb4770925906f7399fd8
Maanil Verma <vermaMaanil97@gmail.com>
2023-01-17 13:47:19 +0530

    feat: Add NavBar Component with Styles

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Button } from "../Button";
+import { BellIcon } from "../../assets/BellIcon";
+import styles from "./NavBar.module.scss";
+
+const NavBar = () => {
+  return (
+    <header className={styles["navbar-container"]}>
+      <h1 className={styles["header-logo"]}>HOURS</h1>
+      <div className={styles["menu-wrapper"]}>
+        <Button className={styles["menu-item"]} text="Dashboard" />
+        <Button className={styles["menu-item"]} text="Projects" />
+        <Button className={styles["menu-item"]} text="Team" />
+        <Button className={styles["menu-item"]} text="Clients" />
+        <Button className={styles["menu-item"]} text="Time" />
+        <Button className={styles["menu-item"]} text="Reports" />
+      </div>
+      <div className={styles["user-details"]}>
+        <BellIcon />
+        <img
+          className={styles["user-profile-image"]}
+          src="src/assets/profile-image.svg"
+          alt="profile image"
+        />
+        <p className={styles["username"]}>Mario</p>
+      </div>
+    </header>
+  );
+};
+
+export { NavBar };
