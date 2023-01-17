8cdb080eeaef4961aa0666a6c94de38e635a91a0
Maanil Verma <vermaMaanil97@gmail.com>
2023-01-17 13:48:54 +0530

    feat: integrate NavBar in App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { NavBar } from "./components/NavBar";
+
 import styles from "./App.module.scss";
 
 function App() {
-  return <div className={styles["App"]}></div>;
+  return (
+    <div className={styles["app"]}>
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;
