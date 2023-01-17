fef2b9b598ad0617d9786ff4cbaf8b5959b45156
Maanil Verma <vermaMaanil97@gmail.com>
2023-01-17 08:50:06 +0530

    chore: convert css to scss module

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import styles from "./App.module.scss";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  )
+  return <div className={styles["App"]}></div>;
 }
 
-export default App
+export default App;
