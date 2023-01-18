c715c61ebfce9f0eed6f0ba6ddf26b437a78b749
Maanil Verma <vermaMaanil97@gmail.com>
2023-01-18 10:19:17 +0530

    chore: Add Employee Data and assets

---
+++ src/data/employeeDetails.js
@@ -0,0 +1,22 @@
+export const employeeDetails = [
+  {
+    employeeName: "Andrea",
+    employeeDesignation: "UX Designer",
+  },
+  {
+    employeeName: "Alvaro",
+    employeeDesignation: "Backend Developer",
+  },
+  {
+    employeeName: "Juan",
+    employeeDesignation: "UX Senior",
+  },
+  {
+    employeeName: "Jose",
+    employeeDesignation: "Marketing",
+  },
+  {
+    employeeName: "Maria",
+    employeeDesignation: "UX Junior",
+  },
+];
