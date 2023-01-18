fd0455ce5dcff2624ea1547b6a09326ca443ecf3
Maanil Verma <vermaMaanil97@gmail.com>
2023-01-18 23:07:59 +0530

    feat: add data for budget card

---
+++ src/data/budgetData.js
@@ -0,0 +1,34 @@
+export const budgetData = [
+  {
+    title: "Insurance App",
+    subText: "Verti",
+    totalBudget: "70.000",
+    profitability: "-2.500",
+    actualHours: "1.100",
+    soldHours: "1.100",
+  },
+  {
+    title: "Neo",
+    subText: "Bankia",
+    totalBudget: "70.000",
+    profitability: "4.000",
+    actualHours: "1.100",
+    soldHours: "1.000",
+  },
+  {
+    title: "VR Website",
+    subText: "Barca",
+    totalBudget: "70.000",
+    profitability: "4.000",
+    actualHours: "1.100",
+    soldHours: "2.000",
+  },
+  {
+    title: "VR Website",
+    subText: "Barca",
+    totalBudget: "70.000",
+    profitability: "4.000",
+    actualHours: "1.100",
+    soldHours: "1.100",
+  },
+];
