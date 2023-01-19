2d9e116cc71ac0f775798a238fc7d2b902b2bf76
Maanil Verma <vermaMaanil97@gmail.com>
2023-01-19 15:38:05 +0530

    feat: Add icon as a prop for button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 
-const Button = ({ className, text }) => {
+const Button = ({ className, text, icon, iconClassName }) => {
   return (
     <button className={className} type="button">
+      {icon && <span className={iconClassName}>{icon}</span>}
       {text}
     </button>
   );
